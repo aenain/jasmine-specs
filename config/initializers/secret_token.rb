b6c5592f4b7266503a59c5779791152e10c12563
Artur Hebda <arturhebda@gmail.com>
2013-08-31 17:25:47 +0200

    Upgrade to Rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-JasmineTests::Application.config.secret_token = 'adfa6640a6e503aacd6581619e1bcb799b9286e19ddde94be4fdbc56b0bd2a8a00c88fb480c7b26e50435fee23a66c4c077a4e7a091d6e1a39e1c9432fea9d3c'
+JasmineTests::Application.config.secret_key_base = 'adfa6640a6e503aacd6581619e1bcb799b9286e19ddde94be4fdbc56b0bd2a8a00c88fb480c7b26e50435fee23a66c4c077a4e7a091d6e1a39e1c9432fea9d3c'
